7009a16db5c62761d35858eb5782d9025e02da66
Kyosuke Fujimoto <kyoro.f@gmail.com>
2022-11-03 17:47:56 +0900

    Set theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,22 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./style.css";
 import App from "./App";
+import "./style.css";
+import { themeOptions } from "./theme";
 
 const container = document.getElementById("root");
 
 const root = createRoot(container!);
 
+const theme = createTheme(themeOptions);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
---
+++ frontend/src/theme.ts
@@ -0,0 +1,16 @@
+import { ThemeOptions } from '@mui/material/styles/';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#3b8e44',
+    },
+    secondary: {
+      main: '#ad4f24',
+    },
+    background: {
+      default: '#fafafa',
+    },
+  },
+};
